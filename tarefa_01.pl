ede2872d161825a32659d71eca63eccab173797e
PedruuH <pedro.inventador@hotmail.com>
2020-11-15 21:37:49 -0300

    atualizado

--- tarefa_01.pl
+++ tarefa_01.pl
@@ -1,3 +1,4 @@
+% Aluno: Pedro Henrique R M Santos 11611ECP017
 % ------- Piratas na Taverna -------
 % https://rachacuca.com.br/logica/problemas/piratas-na-taverna/
 
@@ -7,138 +8,92 @@ resolve(T) :-
                         pirata(_,_,_,_,_,_),
                         pirata(_,_,_,_,_,_),
                         pirata(_,_,_,_,_,_)),
-
-        % O pirata com Tapa-olho é o comandante do navio que tem 15 canhões.
+        % O pirata com Tapaolho é o comandante do navio que tem 15 canhões.
             pirata(A,T),
-            caracteristica(A,tapa-olho),
-            canhoes(A,15-canhoes),
+            caracteristica(A,tapaolho),
+            canhoes(A,15),
 
         % O Irlandês está em algum lugar à direita do pirata de Vermelho.
-            pirata(B,T),
-            nacionalidade(B,irlandes),
-            pirata(C,T),
             algum_lugar_a_direita(B,C,T),
+            nacionalidade(B,irlandes),
             camisa(C,vermelho),
-            
+
         % O pirata de Bandana está ao lado do pirata que comanda uma tripulação de 80 homens.
         %A tripulação de 80 homens enterrou um baú cheio de Esmeraldas.
-            pirata(D,T),
-            pirata(E,T),
+            ao_lado(D,E,T), %erro
             caracteristica(D,bandana),
-            ao_lado(D,E,S),
-            tripulacao(E,80-homens),
+            tripulacao(E,80),
             tesouro(E,esmeraldas),
-
         % Na segunda posição está o pirata que enterrou um baú cheio de Ouro.
-            pirata(F,T),
-            tesouro(F,ouro),
             segunda_posicao(F,T),
-
+            tesouro(F,ouro),
         % O Espanhol está de camisa Azul.
             pirata(G,T),
             nacionalidade(G, espanhol),
             camisa(G,azul),
-
-
         % Na terceira posição está o pirata de camisa Vermelho.
-            pirata(H,T),
             terceira_posicao(H,T),
             camisa(H,vermelho),
-
         % O pirata que tem um Papagaio está exatamente à esquerda do pirata que enterrou um baú cheio de Rubis.
-            pirata(I,T),
-            caracteristica(I,papagaio),
-            pirata(J,T),
             exatamente_a_esquerda(I,J,T),
+            caracteristica(I,papagaio),
             tesouro(J,rubis),
-
         %O pirata de Amarelo está exatamente à esquerda do pirata Irlandês.
-            pirata(K,T),
-            pirata(L,T),
-            camisa(K,amarelo),
             exatamente_a_esquerda(K,L,T),
+            camisa(K,amarelo),
             nacionalidade(L,irlandes),
-
         %O pirata Holandês está ao lado do pirata que comanda o navio que tem 20 canhões.
-            pirata(N,T),
-            nacionalidade(N,holandes),
-            pirata(O,T),
             ao_lado(N,O,T),
-            canhoes(O,20-canhoes),
-
+            nacionalidade(N,holandes),
+            canhoes(O,20),
         %O pirata que comanda o navio com mais canhões está exatamente à direita do pirata que enterrou um baú cheio de Diamantes.
-            pirata(P,T),
-            canhoes(P,30-canhoes),
-            pirata(Q,T),
             exatamente_a_direita(P,Q,T),
+            canhoes(P,30),
             tesouro(Q,diamantes),
-
         %O pirata de Vermelho está ao lado do pirata que comanda o navio que tem 10 canhões.
         % A maior tripulação (160) trabalha no navio com a menor quantidade de canhões(10).
-            pirata(R,T),
-            pirata(S,T),
             ao_lado(R,S,T),
-            tripulação(S,160-homens),
-            canhoes(S,10-canhoes),
-
+            camisa(R,vermelho), %erro
+            tripulacao(S,160),
+            canhoes(S,10),
         %A Bandana está sendo usada pelo pirata de camisa Amarela.
             pirata(U,T),
             caracteristica(U,bandana),
             camisa(U,amarelo),
-
         %O Francês comanda o navio que tem 25 canhões.
         %A tripulação de 100 homens trabalha no navio que tem 25 canhões.
             pirata(V,T),
             nacionalidade(V,frances),
-            canhoes(V,25-canhoes),
-            tripulacao(V,100-homens),
-
-
+            canhoes(V,25),
+            tripulacao(V,100),
         %O pirata de Perna de pau está ao lado do pirata que lidera uma tripulação de 140 homens.
-            pirata(W,T),
-            caracteristica(W,pernadepau),
-            pirata(X,T),
             ao_lado(W,X,T),
-            tripulacao(X,140-homens),
-
-            
-
+            caracteristica(W,pernadepau),
+            tripulacao(X,140),
         %O Inglês está exatamente à direita do pirata que enterrou um baú cheio de Prata.
-            pirata(AB,T),
-            nacionalidade(AB,ingles),
-            pirata(AC,T),
             exatamente_a_direita(AB,AC,T),
+            nacionalidade(AB,ingles),
             tesouro(AC,prata),
         %O navio que tem 20 canhões é comandado pelo pirata de Perna de pau.
             pirata(AE,T),
             caracteristica(AE,pernadepau),
-            canhoes(AE,20-canhoes),
+            canhoes(AE,20),
         %O pirata Inglês está exatamente à esquerda do pirata que comanda uma tripulação de 100 homens.
-            pirata(AF,T),
-            pirata(AG,T),
-            nacionalidade(AF,ingles),
-            tripulacao(AG,100-homens),
             exatamente_a_esquerda(AF,AG,T),
+            nacionalidade(AF,ingles),
+            tripulacao(AG,100),
         %O pirata de camiseta Branca está ao lado do pirata que enterrou um baú cheio de Prata.
-            pirata(AH,T),
-            pirata(AI,T),
-            camisa(AH,branca),
             ao_lado(AH,AI,T),
+            camisa(AH,branca),
             tesouro(AI,prata),
-
-            
-            pirata(AG,T),
-            canhoes(AG,100-homens),
-            pirata(AK,T),
-            camisa(AK,verde),
-            pirata(AL,T),
-            tripulacao(AL,120-homens),
-
-
+        
+        %fechamento
             pirata(AW,T),
-            caracteristica(AW,cicatriz).
-
-            
+            caracteristica(AW,cicatriz),
+            pirata(AL,T),
+            tripulacao(AL,120),
+            pirata(AK,T),
+            camisa(AK,verde).
 
 
 pirata(X,taverna(X,_,_,_,_)).
@@ -146,7 +101,6 @@ pirata(X,taverna(_,X,_,_,_)).
 pirata(X,taverna(_,_,X,_,_)).
 pirata(X,taverna(_,_,_,X,_)).
 pirata(X,taverna(_,_,_,_,X)).
-
 
 camisa(pirata(X,_,_,_,_,_),X).
 nacionalidade(pirata(_,X,_,_,_,_),X).
